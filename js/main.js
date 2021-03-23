2fde898d681225d4d54109f605e63944adfa3ba8
YanaHorbachova <maya69877@gmail.com>
2021-03-23 20:34:56 +0200

    Создание и рендер разметки по массиву данных и предоставленному шаблону

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 import images from "./gallery-items.js";
+const ulRef = document.querySelector(".js-gallery");
+
+function createList() {
+  const liToAdd = [];
+  images.forEach((image) => {
+    const liRef = document.createElement("li");
+    liRef.classList.add("gallery__item");
+    const linkRef = document.createElement("a");
+    linkRef.classList.add("gallery__link");
+    linkRef.setAttribute("href", image.original);
+    liRef.appendChild(linkRef);
+    const imgRef = document.createElement("img");
+    imgRef.classList.add("gallery__image");
+    imgRef.setAttribute("src", image.preview);
+    imgRef.setAttribute("data-source", image.original);
+    imgRef.setAttribute("alt", image.description);
+    linkRef.appendChild(imgRef);
+    liToAdd.push(liRef);
+  });
+  ulRef.append(...liToAdd);
+}
+createList();
+
