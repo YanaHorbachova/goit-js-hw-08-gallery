0e88eb22694b30729a3eb0030b695de45b52468c
YanaHorbachova <maya69877@gmail.com>
2021-03-23 20:47:52 +0200

    Открытие модального окна по клику на элементе галереи

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 import images from "./gallery-items.js";
 const ulRef = document.querySelector(".js-gallery");
+const modalRef = document.querySelector(".js-lightbox");
+const buttonRef = document.querySelector(
+  'button[data-action="close-lightbox"]'
+);
+const modalImgRef = document.querySelector(".lightbox__image");
+const overlayRef = document.querySelector(".lightbox__overlay");
 
 function createList() {
   const liToAdd = [];
@@ -22,3 +28,17 @@ function createList() {
 }
 createList();
 
+ulRef.addEventListener("click", openModal);
+// buttonRef.addEventListener("click", closeModal);
+
+
+function openModal(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== "IMG") {
+      return;
+    }
+    modalImgRef.setAttribute("src", event.target.dataset.source);
+    modalImgRef.setAttribute("alt", event.target.alt);
+  
+    modalRef.classList.add("is-open");
+  }
